d9dc61be31ef9a5e3d41edf66f83e4de0e7ef8dc
joseernanejob <jose.ernane.job@gmail.com>
2024-02-13 11:10:20 -0300

    feat: add handleHttpMessageNotReadableException and handleInvalidDataException

--- src/main/java/com/rocketseat/certification_nlw/exceptions/HandlerException.java
+++ src/main/java/com/rocketseat/certification_nlw/exceptions/HandlerException.java
@@ -2,8 +2,13 @@ package com.rocketseat.certification_nlw.exceptions;
 
 import java.util.Date;
 
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -11,6 +16,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class HandlerException extends ResponseEntityExceptionHandler {
+
+  @Override
+  protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+      HttpHeaders headers,
+      HttpStatusCode status, WebRequest req) {
+    ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), req.getDescription(false));
+    return new ResponseEntity<>(response,
+        HttpStatus.BAD_REQUEST);
+  }
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ExceptionResponse> handleInternal(Exception ex, WebRequest req) {
@@ -32,6 +46,13 @@ public class HandlerException extends ResponseEntityExceptionHandler {
   public ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException ex, WebRequest req) {
     ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), req.getDescription(false));
     return new ResponseEntity<ExceptionResponse>(response, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(InvalidDataAccessApiUsageException.class)
+  public ResponseEntity<ExceptionResponse> handleInvalidDataException(InvalidDataAccessApiUsageException ex,
+      WebRequest req) {
+    ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), req.getDescription(false));
+    return new ResponseEntity<ExceptionResponse>(response, HttpStatus.BAD_REQUEST);
   }
 
 }
