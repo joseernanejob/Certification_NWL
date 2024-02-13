45a0b7a13732931dff8edeccd7698485cda2a948
joseernanejob <jose.ernane.job@gmail.com>
2024-02-13 11:13:33 -0300

    remove: remove unused imports

--- src/main/java/com/rocketseat/certification_nlw/exceptions/HandlerException.java
+++ src/main/java/com/rocketseat/certification_nlw/exceptions/HandlerException.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
