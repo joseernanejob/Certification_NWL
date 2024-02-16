7f6d7dfe1988652b7c8563f7314b02e7ce42e638
joseernanejob <jose.ernane.job@gmail.com>
2024-02-16 10:43:14 -0300

    change: change return  findByEmailAndTechnology

--- src/main/java/com/rocketseat/certification_nlw/modules/students/repositories/CertificationRepository.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/repositories/CertificationRepository.java
@@ -1,6 +1,6 @@
 package com.rocketseat.certification_nlw.modules.students.repositories;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,5 +11,5 @@ import com.rocketseat.certification_nlw.modules.students.entities.CertificationE
 public interface CertificationRepository extends JpaRepository<CertificationEntity, UUID> {
 
   @Query("SELECT c FROM certifications c INNER JOIN c.student std WHERE std.email = :email AND c.technology = :technology")
-  public Optional<CertificationEntity> findByEmailAndTechnology(String email, String technology);
+  public List<CertificationEntity> findByEmailAndTechnology(String email, String technology);
 }
